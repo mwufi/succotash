1b3321b6ef6caf1bd3f883210d9f3827adebed1a
Z. Tang <chuang.tang@yale.edu>
2019-11-17 14:49:58 +0000

    Adds logging to module forward pass
    Base model which will report itself :)))

--- models/basic.py
+++ models/basic.py
@@ -1,19 +1,71 @@
+import logging
 from typing import Any
 
+from numpy import prod
 from torch import nn
 
+format_strings = {
+	'date': '%(asctime)s:%(msecs)03d - %(name)s - %(message)s',
+	'basic': ':%(msecs)03d - %(name)s - %(message)s'
+}
 
-class MLP(nn.Module):
+logging.basicConfig(
+	format=format_strings['basic'],
+	level=logging.INFO,
+	datefmt='%m/%d/%Y %I:%M:%S'
+)
+
+
+class ModelBase(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.logger = logging.getLogger(self.name)
+
+	@property
+	def trainable_parameters(self):
+		model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+		params = sum([prod(p.size()) for p in model_parameters])
+		return params
+
+	def preprocess(self, *input):
+		for t in input:
+			self.logger.info(f'input size: {t.size()}')
+		return input
+
+	def postprocess(self, tensor_outputs):
+		self.logger.info(f'output size: {tensor_outputs.size()}')
+		return tensor_outputs
+
+	def forward(self, *input: Any, **kwargs: Any):
+		tensor_inputs = self.preprocess(*input)
+		tensor_outputs = self._forward(*tensor_inputs, **kwargs)
+		return self.postprocess(tensor_outputs)
+
+
+class MLP(ModelBase):
+	name = 'MLP'
+
 	def __init__(self, layer_sizes):
+		super().__init__()
+
 		if len(layer_sizes) < 1:
 			raise ValueError('You have to have an input dimension')
 		if len(layer_sizes) < 2:
 			raise ValueError('You have to have an output dimension')
 
-		self.layers = nn.Sequential(*[
-			nn.Linear(layer_sizes[i], layer_sizes[i + 1])
+		f = [
+			nn.Linear(in_features=layer_sizes[i], out_features=layer_sizes[i + 1])
 			for i in range(len(layer_sizes) - 1)
-		])
+		]
 
-	def forward(self, tensor: Any, **kwargs: Any):
+		self.layers = nn.Sequential(*f)
+
+		print()
+		print(f'(init)'.center(40, '-'))
+		print(f'{self.name}')
+		print(f'{self.trainable_parameters} trainable parameters')
+		print('(end of init)'.center(40, '-'))
+		print()
+
+	def _forward(self, tensor: Any):
 		return self.layers(tensor)
--- run_training.py
+++ run_training.py
@@ -1,10 +1,10 @@
 import torch
 
 from models.basic import MLP
-from utils import test_config
 
+B = 13
 H = 40
-W = 40
+W = 2
 
 
 def print_diagnostics():
@@ -13,15 +13,15 @@ def print_diagnostics():
 
 
 def create_model():
-	x = MLP([H * W, 100, 10])
-	print(x)
+	x = MLP([W*H, 10])
 	return x
 
 
 if __name__ == "__main__":
 	print_diagnostics()
-	test_config()
-	# x = torch.rand((H, W), names=('H', 'W'))
-	# model = create_model()
-	# print(x)
-	# print(model(x))
+
+	x = torch.rand((B, H, W), names=('batch_size', 'height', 'width'))
+	z = x.reshape((B, H*W))
+	model = create_model()
+
+	t = model(z)
